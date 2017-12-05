cad1fe1f547b4d481d6990234541fb6ce6646222
miriam2019 <34288391+miriam2019@users.noreply.github.com>
2017-12-05 15:28:43 -0800

    Create miriam.js

---
+++ miriam.js
@@ -0,0 +1,450 @@
+
+//move to the starting position for drawing the flowers
+penUp();
+moveTo(0, 448);
+penDown();
+
+hide();
+
+
+function drawFlower() {
+  
+for (var i = 0; i < 4; i++) {
+  drawStem();
+  drawBud();
+  
+  }
+}
+
+drawFlower();
+
+function drawStem(){
+penColor("green");
+moveForward();
+drawLeaf();
+}
+
+function drawLeaf() {
+ turnLeft(68);
+ arcLeft(90, 12);
+ turnLeft(75);
+ arcLeft(95,12);
+ //arcLeft(90, 25);
+ turnRight(9);
+ turnLeft(140);
+  moveForward(10);
+  turnLeft();
+  turnLeft();
+  moveForward(9);
+  turnLeft(84);
+  moveForward(20);
+  turnRight(3);
+  
+}
+
+ 
+function drawColumnOfFlowers(){
+  drawBud();
+  turnRight();
+  turnRight();
+  moveForward(180);
+  turnLeft();
+  moveForward(9);
+  turnLeft(92.5);
+  drawFlower();
+  
+  
+   drawBud();
+  turnRight();
+  turnRight();
+  moveForward(152);
+  turnLeft();
+  moveForward(9);
+  turnLeft();
+  drawFlower();
+  
+  drawBud();
+  turnRight();
+  turnRight();
+  moveForward(180);
+  turnLeft();
+  moveForward(9);
+  turnLeft();
+  drawFlower();
+  
+  
+   drawBud();
+  turnRight();
+  turnRight();
+  moveForward(152);
+  turnLeft();
+  moveForward(9);
+  turnLeft();
+  drawFlower();
+  
+   drawBud();
+  turnRight();
+  turnRight();
+  moveForward(180);
+  turnLeft();
+  moveForward(9);
+  turnLeft();
+  drawFlower();
+  
+  
+   drawBud();
+  turnRight();
+  turnRight();
+  moveForward(152);
+  turnLeft();
+  moveForward(9);
+  turnLeft();
+  drawFlower();
+  
+  drawBud();
+  turnRight();
+  turnRight();
+  moveForward(180);
+  turnLeft();
+  moveForward(9);
+  turnLeft();
+  drawFlower();
+  
+  
+   drawBud();
+  turnRight();
+  turnRight();
+  moveForward(152);
+  turnLeft();
+  moveForward(9);
+  turnLeft();
+  drawFlower();
+  
+   drawBud();
+  turnRight();
+  turnRight();
+  moveForward(180);
+  turnLeft();
+  moveForward(9);
+  turnLeft();
+  drawFlower();
+  
+  
+   drawBud();
+  turnRight();
+  turnRight();
+  moveForward(152);
+  turnLeft();
+  moveForward(9);
+  turnLeft();
+  drawFlower();
+  
+  drawBud();
+  turnRight();
+  turnRight();
+  moveForward(180);
+  turnLeft();
+  moveForward(9);
+  turnLeft();
+  drawFlower();
+  
+  
+   drawBud();
+  turnRight();
+  turnRight();
+  moveForward(152);
+  turnLeft();
+  moveForward(9);
+  turnLeft();
+  drawFlower();
+  
+   drawBud();
+  turnRight();
+  turnRight();
+  moveForward(180);
+  turnLeft();
+  moveForward(9);
+  turnLeft();
+  drawFlower();
+  
+  
+   drawBud();
+  turnRight();
+  turnRight();
+  moveForward(152);
+  turnLeft();
+  moveForward(9);
+  turnLeft();
+  drawFlower();
+  
+  drawBud();
+  turnRight();
+  turnRight();
+  moveForward(180);
+  turnLeft();
+  moveForward(9);
+  turnLeft();
+  drawFlower();
+  
+  
+   drawBud();
+  turnRight();
+  turnRight();
+  moveForward(152);
+  turnLeft();
+  moveForward(9);
+  turnLeft();
+  drawFlower();
+  
+   drawBud();
+  turnRight();
+  turnRight();
+  moveForward(180);
+  turnLeft();
+  moveForward(9);
+  turnLeft();
+  drawFlower();
+  
+  
+   drawBud();
+  turnRight();
+  turnRight();
+  moveForward(152);
+  turnLeft();
+  moveForward(9);
+  turnLeft();
+  drawFlower();
+  
+  drawBud();
+  turnRight();
+  turnRight();
+  moveForward(180);
+  turnLeft();
+  moveForward(9);
+  turnLeft();
+  drawFlower();
+  
+  
+   drawBud();
+  turnRight();
+  turnRight();
+  moveForward(152);
+  turnLeft();
+  moveForward(9);
+  turnLeft();
+  drawFlower();
+  
+   drawBud();
+  turnRight();
+  turnRight();
+  moveForward(180);
+  turnLeft();
+  moveForward(9);
+  turnLeft();
+  drawFlower();
+  
+  
+   drawBud();
+  turnRight();
+  turnRight();
+  moveForward(152);
+  turnLeft();
+  moveForward(9);
+  turnLeft();
+  drawFlower();
+  
+  drawBud();
+  turnRight();
+  turnRight();
+  moveForward(180);
+  turnLeft();
+  moveForward(9);
+  turnLeft();
+  drawFlower();
+  
+  
+   drawBud();
+  turnRight();
+  turnRight();
+  moveForward(152);
+  turnLeft();
+  moveForward(9);
+  turnLeft();
+  drawFlower();
+  
+   drawBud();
+  turnRight();
+  turnRight();
+  moveForward(180);
+  turnLeft();
+  moveForward(9);
+  turnLeft();
+  drawFlower();
+  
+  
+   drawBud();
+  turnRight();
+  turnRight();
+  moveForward(152);
+  turnLeft();
+  moveForward(9);
+  turnLeft();
+  drawFlower();
+  
+  drawBud();
+  turnRight();
+  turnRight();
+  moveForward(180);
+  turnLeft();
+  moveForward(9);
+  turnLeft();
+  drawFlower();
+  
+  
+   drawBud();
+  turnRight();
+  turnRight();
+  moveForward(152);
+  turnLeft();
+  moveForward(9);
+  turnLeft();
+  drawFlower();
+  
+   drawBud();
+  turnRight();
+  turnRight();
+  moveForward(180);
+  turnLeft();
+  moveForward(9);
+  turnLeft();
+  drawFlower();
+  
+  
+   drawBud();
+  turnRight();
+  turnRight();
+  moveForward(152);
+  turnLeft();
+  moveForward(9);
+  turnLeft();
+  drawFlower();
+  
+  drawBud();
+  turnRight();
+  turnRight();
+  moveForward(180);
+  turnLeft();
+  moveForward(9);
+  turnLeft();
+  drawFlower();
+  
+  
+   drawBud();
+  turnRight();
+  turnRight();
+  moveForward(152);
+  turnLeft();
+  moveForward(9);
+  turnLeft();
+  drawFlower();
+  
+   drawBud();
+  turnRight();
+  turnRight();
+  moveForward(180);
+  turnLeft();
+  moveForward(9);
+  turnLeft();
+  drawFlower();
+  
+  
+   drawBud();
+  turnRight();
+  turnRight();
+  moveForward(152);
+  turnLeft();
+  moveForward(9);
+  turnLeft();
+  drawFlower();
+  
+  drawBud();
+  turnRight();
+  turnRight();
+  moveForward(180);
+  turnLeft();
+  moveForward(9);
+  turnLeft();
+  drawFlower();
+  
+  
+   drawBud();
+  turnRight();
+  turnRight();
+  moveForward(152);
+  turnLeft();
+  moveForward(9);
+  turnLeft();
+  drawFlower();
+  
+   drawBud();
+  turnRight();
+  turnRight();
+  moveForward(180);
+  turnLeft();
+  moveForward(9);
+  turnLeft();
+  drawFlower();
+  
+  
+   drawBud();
+  turnRight();
+  turnRight();
+  moveForward(152);
+  turnLeft();
+  moveForward(9);
+  turnLeft();
+  drawFlower();
+  
+  drawBud();
+  turnRight();
+  turnRight();
+  moveForward(180);
+  turnLeft();
+  moveForward(9);
+  turnLeft();
+  drawFlower();
+  
+  
+   drawBud();
+  turnRight();
+  turnRight();
+  moveForward(152);
+  turnLeft();
+  moveForward(9);
+  turnLeft();
+  drawFlower();
+
+}
+
+function drawBud(){
+  penRGB(randomNumber(0, 255), randomNumber(0, 255), randomNumber(0, 255), 1);dot(6);
+  //turnRight(45);
+ //moveForward(3);
+ //dot(5);
+ //turnLeft();
+//turnLeft(90);
+///moveForward(5);
+///dot(5);
+}
+
+
+drawColumnOfFlowers();
+
+
+
+
+
+
+
+
+
