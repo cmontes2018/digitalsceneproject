095acc157c6429ff209f432028f62d5bca0f65c9
miriam2019 <34288391+miriam2019@users.noreply.github.com>
2017-12-05 16:49:45 -0800

    Update miriam.js

--- miriam.js
+++ miriam.js
@@ -1,109 +1,59 @@
 
 //move to the starting position for drawing the flowers
 penUp();
-moveTo(0, 448);
+moveTo(160, 448);
 penDown();
 
 hide();
 
-//draws one flower
-function drawFlower() {
-  
-for (var i = 0; i < 4; i++) {
-  drawStem();
-  drawBud();
-  
-  }
-}
+//draws all the flowers
+drawAllFlowers();
 
-drawFlower();
-
+//function inside the leaf
 function drawStem(){
 penColor("green");
 moveForward();
 drawLeaf();
 }
 
+//function for drawing the leaf
 function drawLeaf() {
  turnLeft(68);
  arcLeft(90, 12);
  turnLeft(75);
  arcLeft(95,12);
- //arcLeft(90, 25);
  turnRight(9);
  turnLeft(140);
-  moveForward(10);
-  turnLeft();
-  turnLeft();
-  moveForward(9);
-  turnLeft(84);
-  moveForward(20);
-  turnRight(3);
-  
+moveForward(10);
+turnLeft();
+turnLeft();
+moveForward(9);
+turnLeft(84);
+moveForward(20);
+turnRight(3);
 }
 
- 
-function drawColumnOfFlowers(){
-  turnRight();
-  turnRight();
-  moveForward(180);
-  turnLeft();
-  moveForward(9);
-  turnLeft(92.5);
-  drawFlower();
-  
- for (var i = 0; i < 10; i++) {
+//function for drawing all the flowers
+function drawAllFlowers(){
+  for (var i = 0; i < 390; i++) {
+    drawFlower();
+    moveTo(randomNumber(0, 320), randomNumber(300,490));
     
-  turnRight();
-  turnRight();
-  moveForward(180);
-  turnLeft();
-  moveForward(9);
-  turnLeft();
-  drawFlower();
-  
-  turnRight();
-  turnRight();
-  moveForward(152);
-  turnLeft();
-  moveForward(9);
-  turnLeft();
-  drawFlower();
-  
-  
-  turnRight();
-  turnRight();
-  moveForward(180);
-  turnLeft();
-  moveForward(9);
-  turnLeft();
-  drawFlower();
-  
-  
-  
-  turnRight();
-  turnRight();
-  moveForward(152);
-  turnLeft();
-  moveForward(9);
-  turnLeft();
-  drawFlower();
   }
-   
+}
+//draws one flower
+function drawFlower() {
+  turnTo(randomNumber(-10, 10));
   
-
+  penDown();
+  drawStem();
+  drawBud();
+  penUp();
 }
-
+  
+//draws the actual bulb on the flower
 function drawBud(){
-  penRGB(randomNumber(0, 255), randomNumber(0, 255), randomNumber(0, 255), 1);dot(6);
-  //turnRight(45);
- //moveForward(3);
- //dot(5);
- //turnLeft();
-//turnLeft(90);
-///moveForward(5);
-///dot(5);
+  penRGB(randomNumber(0, 255), randomNumber(0, 255), randomNumber(0, 255), 1);
+  dot(6);
+ 
 }
-
-
-drawColumnOfFlowers();
