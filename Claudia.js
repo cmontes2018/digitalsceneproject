c79879ba6f918bc77f961ed736668cbdefb6ce4f
cmontes2018 <34288346+cmontes2018@users.noreply.github.com>
2017-12-05 16:55:36 -0800

    Create Claudia.js

---
+++ Claudia.js
@@ -0,0 +1,47 @@
+function drawSky() {
+  drawSkyLayers();
+  drawAllClouds();
+}
+drawSky();
+//draws one sky layer 
+function drawSkyLayer(r, g, b, a) {
+  for (var i = 0; i < 50; i++) {
+    hide();
+    penUp();
+    moveTo(randomNumber(0, 320), randomNumber(0, 300));
+    penRGB(r, g, b, a);
+    dot(10);
+  }
+}
+//Draws the layers of the sky 
+function drawSkyLayers() {
+  for (var i = 0; i < 50; i++) {
+    drawSkyLayer(randomNumber(80, 90), randomNumber(110, 190), randomNumber(225, 255), 0.5);
+  }
+}
+drawSkyLayers();
+//draws one cloud
+function drawCloud(r, g, b, a) {
+  for (var i = 0; i < 1; i++) {
+    arcLeft(90, 25);
+    moveTo(randomNumber(0, 321), randomNumber(0, 171));
+    penRGB(r, g, b, a);
+    dot(15);
+    penUp();
+    move(20, 0);
+    penDown();
+    penUp();
+    dot(20);
+    move(20, 0);
+    penDown();
+    dot(15);
+    penUp();
+  }
+}
+//draws all clouds 
+function drawAllClouds() {
+  for (var i = 0; i < 7; i++) {
+    drawCloud(242, 244, 247, 1);
+  }
+}
+drawAllClouds();
